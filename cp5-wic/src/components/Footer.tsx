1de3e1eda4082466eda7864f837ef37b10f626aa
Samir Neto <samihneto@gmail.com>
2024-10-04 08:04:03 -0300

    Criação dos componentes Header e Footer

---
+++ cp5-wic/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer>
+        <h2>Criado por:</h2>
+        <ul>
+            <li>Felipe Levy</li>
+            <li>Samir Neto</li>
+        </ul>
+    </footer>
+  )
+}
---
+++ cp5-wic/src/components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Worlds in Collision</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Resumo da Obra</li>
+                    <li>Sobre o autor</li>
+                    <li>Teoria</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
