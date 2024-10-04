47649f9bc46457a5cc846906234857533e8e0ec8
Samir Neto <samihneto@gmail.com>
2024-10-04 08:19:41 -0300

    Estabelecimento de links em Header

--- cp5-wic/src/components/Header.tsx
+++ cp5-wic/src/components/Header.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
         <header>
             <h1>Worlds in Collision</h1>
             <nav>
                 <ul>
-                    <li>Home</li>
-                    <li>Resumo da Obra</li>
-                    <li>Sobre o autor</li>
-                    <li>Teoria</li>
-                    <li>API Nasa</li>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/">Resumo da Obra</Link></li>
+                    <li><Link href="/">Sobre o autor</Link></li>
+                    <li><Link href="/">Teoria</Link></li>
+                    <li><Link href="/">API Nasa</Link></li>
                 </ul>
             </nav>
         </header>
